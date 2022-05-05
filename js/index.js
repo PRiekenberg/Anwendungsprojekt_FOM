2721ba51cb43c0bc9f5182b175888a7cd57361dc
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-05 16:24:22 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ var myHeading = document.querySelector('h1');
 myHeading.textContent = "JavaScript funktioniert!";
 
 function set_h1(){
-  document.getElementByTagName('h1').innerHTML = "Kann ich nur bestätigen";
+  getElementsByTagName('h1').innerHTML = "Kann ich nur bestätigen";
 }
