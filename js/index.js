bbc83f45cdc2c7373c9b0fcfd8ecf8ac29e2c020
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-05 16:43:44 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,3 +6,21 @@ function set_h1(){
   var headline = document.querySelector('h1');
   headline.textContent = "Kann ich nur bestätigen!";
 }
+function get_cookie(){
+  var cookieList = (document.cookie) ? document.cookie.split(';') : [];
+  var cookieValues = {};
+  for (var i = 0, n = cookieList.length; i != n; ++i) {
+    var cookie = cookieList[i];
+    var f = cookie.indexOf('=');
+    if (f >= 0) {
+      var cookieName = cookie.substring(0, f);
+      var cookieValue = cookie.substring(f + 1);
+	
+	    console.log ("cookieName + " + cookieName + " cookieValue " + cookieValue);
+    
+      if (!cookieValues.hasOwnProperty(cookieName)) {
+        cookieValues[cookieName] = cookieValue;
+      }
+    }
+  }
+}
