c5e004d7b89f6a2a1a3f23dacc8b491526cf625c
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-05 17:44:35 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,32 +7,11 @@ function set_first_cookie(){
 	a = new Date(a.getTime() +1000*60*60*24*365);
 	document.cookie = 'counter=0; expires='+ a.toGMTString()+';'; 
 }
-
 function update_cookie(new_value){
 	var a = new Date();
 	a = new Date(a.getTime() +1000*60*60*24*365);
 	var counter_value = 'counter='+new_value+';';
 	document.cookie = counter_value +' expires='+ a.toGMTString()+';'; 
-}
-function get_cookie(){
-  	
-		
-	else{
- 	 	for (var i = 0, n = cookieList.length; i != n; ++i) {
-    			var cookie = cookieList[i];
-    			var f = cookie.indexOf('=');
-    			if (f >= 0) {
-      				var cookieName = cookie.substring(0, f);
-      				var cookieValue = cookie.substring(f + 1);
-	
-	    			console.log ("cookieName + " + cookieName + " cookieValue " + cookieValue);
-    
-      				if (!cookieValues.hasOwnProperty(cookieName)) {
-        				cookieValues[cookieName] = cookieValue;
-      				}
-    			}
-  		}
-	}
 }
 function increment_counter(){
 	var cookieList = (document.cookie) ? document.cookie.split(';') : [];
