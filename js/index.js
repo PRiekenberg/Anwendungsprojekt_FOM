cd1a151830c61a9115ed8418d85c9aa077f0a453
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-05 18:03:26 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,6 @@ function set_h1(){
   var headline = document.querySelector('h1');
   headline.textContent = "JavaScript funktioniert!";
 }
-function set_first_cookie(){
-	var a = new Date();
-	a = new Date(a.getTime() +1000*60*60*24*365);
-	document.cookie = 'counter=0; expires='+ a.toGMTString()+';'; 
-}
 function update_cookie(new_value){
 	var a = new Date();
 	a = new Date(a.getTime() +1000*60*60*24*365);
@@ -14,27 +9,28 @@ function update_cookie(new_value){
 	document.cookie = counter_value +' expires='+ a.toGMTString()+';'; 
 }
 function increment_counter(){
-	var cookieList = (document.cookie) ? document.cookie.split(';') : [];
-  	var cookieValues = {};
-	if (cookieList.length < 1){
-		set_first_cookie(); 
-	}
-	else{
-		for (var i = 0, n = cookieList.length; i != n; ++i) {
-    			var cookie = cookieList[i];
-    			var f = cookie.indexOf('=');
-    			if (f >= 0) {
-      				var cookieName = cookie.substring(0, f);
-      				var cookieValue = cookie.substring(f + 1);
+	var found = new Boolean;
+	found = false;
+	
+	for (var i = 0, n = cookieList.length; i != n; ++i) {
+    		var cookie = cookieList[i];
+    		var f = cookie.indexOf('=');
+    		if (f >= 0) {
+      			var cookieName = cookie.substring(0, f);
+      			var cookieValue = cookie.substring(f + 1);
 				
-				if cookieName == "counter"{
-					var new_value = parseInt(cookieValue) + 1;
-					update_cookie(new_value);
-				}
-    			}
-  		}
+			if (cookieName == "counter"){
+				var new_value = parseInt(cookieValue) + 1;
+				update_cookie(new_value);
+				found = true;
+			}
+    		}
+  	}
+	if (found == false){
+		update_cookie(1);
 	}
 }
+
 function read_counter_cookie(){
 	var cookieList = (document.cookie) ? document.cookie.split(';') : [];
   	var cookieValues = {};
@@ -45,7 +41,7 @@ function read_counter_cookie(){
       				var cookieName = cookie.substring(0, f);
       				var cookieValue = cookie.substring(f + 1);
 				
-				if cookieName == "counter"{
+				if (cookieName == "counter"){
 					var value = parseInt(cookieValue) + 1;
 					var counter = document.getElementByID('counter');
 					counter.textContent = value;
