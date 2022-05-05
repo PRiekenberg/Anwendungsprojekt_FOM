16c770eaa25ae9af08508676ae5ae83e3d511c6c
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-05 16:48:33 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,20 +7,26 @@ function set_h1(){
   headline.textContent = "Kann ich nur bestätigen!";
 }
 function get_cookie(){
-  var cookieList = (document.cookie) ? document.cookie.split(';') : [];
-  var cookieValues = {};
-  for (var i = 0, n = cookieList.length; i != n; ++i) {
-    var cookie = cookieList[i];
-    var f = cookie.indexOf('=');
-    if (f >= 0) {
-      var cookieName = cookie.substring(0, f);
-      var cookieValue = cookie.substring(f + 1);
+  	var cookieList = (document.cookie) ? document.cookie.split(';') : [];
+  	var cookieValues = {};
+	if cookieList.length < 1){
+		document.cookie = 'counter=0;'; 
+	}
+		
+	else{
+ 	 	for (var i = 0, n = cookieList.length; i != n; ++i) {
+    			var cookie = cookieList[i];
+    			var f = cookie.indexOf('=');
+    			if (f >= 0) {
+      				var cookieName = cookie.substring(0, f);
+      				var cookieValue = cookie.substring(f + 1);
 	
-	    console.log ("cookieName + " + cookieName + " cookieValue " + cookieValue);
+	    			console.log ("cookieName + " + cookieName + " cookieValue " + cookieValue);
     
-      if (!cookieValues.hasOwnProperty(cookieName)) {
-        cookieValues[cookieName] = cookieValue;
-      }
-    }
-  }
+      				if (!cookieValues.hasOwnProperty(cookieName)) {
+        				cookieValues[cookieName] = cookieValue;
+      				}
+    			}
+  		}
+	}
 }
