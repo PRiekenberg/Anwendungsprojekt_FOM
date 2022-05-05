27cc41d239a1bf0eef51f5d80c013c41a54ca1d8
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-05 17:40:58 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,16 +5,17 @@ function set_h1(){
 function set_first_cookie(){
 	var a = new Date();
 	a = new Date(a.getTime() +1000*60*60*24*365);
-	document.cookie = 'meincookie=meinwert; expires='+ 
-                  a.toGMTString()+';'; 
+	document.cookie = 'counter=0; expires='+ a.toGMTString()+';'; 
+}
+
+function update_cookie(new_value){
+	var a = new Date();
+	a = new Date(a.getTime() +1000*60*60*24*365);
+	var counter_value = 'counter='+new_value+';';
+	document.cookie = counter_value +' expires='+ a.toGMTString()+';'; 
 }
 function get_cookie(){
-	set_first_cookie();
-  	var cookieList = (document.cookie) ? document.cookie.split(';') : [];
-  	var cookieValues = {};
-	if (cookieList.length < 1){
-		document.cookie = 'counter=0;'; 
-	}
+  	
 		
 	else{
  	 	for (var i = 0, n = cookieList.length; i != n; ++i) {
@@ -33,4 +34,25 @@ function get_cookie(){
   		}
 	}
 }
-
+function increment_counter(){
+	var cookieList = (document.cookie) ? document.cookie.split(';') : [];
+  	var cookieValues = {};
+	if (cookieList.length < 1){
+		set_first_cookie(); 
+	}
+	else{
+		for (var i = 0, n = cookieList.length; i != n; ++i) {
+    			var cookie = cookieList[i];
+    			var f = cookie.indexOf('=');
+    			if (f >= 0) {
+      				var cookieName = cookie.substring(0, f);
+      				var cookieValue = cookie.substring(f + 1);
+				
+				if cookieName = "counter"{
+					var new_value = parseInt(cookieValue) + 1;
+					update_cookie(new_value);
+				}
+    			}
+  		}
+	}
+}
