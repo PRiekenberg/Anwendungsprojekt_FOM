994cd25d92cf0a499ced958d46341941e373f183
PRiekenberg <priekenberg@oelschlaeger.de>
2022-05-10 09:23:34 +0200

    Update test.php

--- test.php
+++ test.php
@@ -10,55 +10,49 @@
     // create the mongo connection object
     $client = new MongoDB\Client($connection_url);
     $collection = $client->anwendungsprojektdb->anwendungsprojektdb;
+    $db = $client->anwendungsprojektdb
     
     $result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
     
     echo "Inserted with Object ID '{$result->getInsertedId()}'";
 
-   /* 
-    // extract the DB name from the connection path
-    $url = parse_url($connection_url);
-    $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
+    try {
+      echo "<h2>Collections</h2>";
+      echo "<ul>";
 
-    // use the database we connected to
-    $db = $m->selectDB($db_name);
-
-    echo "<h2>Collections</h2>";
-    echo "<ul>";
-
-    // print out list of collections
-    $cursor = $db->listCollections();
-    $collection_name = "";
-    foreach( $cursor as $doc ) {
-      echo "<li>" .  $doc->getName() . "</li>";
-      $collection_name = $doc->getName();
-    }
-    echo "</ul>";
-
-    // print out last collection
-    if ( $collection_name != "" ) {
-      $collection = $db->selectCollection($collection_name);
-      echo "<h2>Documents in ${collection_name}</h2>";
-
-      // only print out the first 5 docs
-      $cursor = $collection->find();
-      $cursor->limit(5);
-      echo $cursor->count() . ' document(s) found. <br/>';
+      // print out list of collections
+      $cursor = $db->listCollections();
+      $collection_name = "";
       foreach( $cursor as $doc ) {
-        echo "<pre>";
-        var_dump($doc);
-        echo "</pre>";
+        echo "<li>" .  $doc->getName() . "</li>";
+        $collection_name = $doc->getName();
       }
-    }
+      echo "</ul>";
 
-    // disconnect from server
-    $m->close();
-  } catch ( MongoConnectionException $e ) {
-    die('Error connecting to MongoDB server');
-  } catch ( MongoException $e ) {
-    die('Mongo Error: ' . $e->getMessage());
-  } catch ( Exception $e ) {
-    die('Error: ' . $e->getMessage());
-  }
-  */
+      // print out last collection
+      if ( $collection_name != "" ) {
+        $collection = $db->selectCollection($collection_name);
+        echo "<h2>Documents in ${collection_name}</h2>";
+
+        // only print out the first 5 docs
+        $cursor = $collection->find();
+        $cursor->limit(5);
+        echo $cursor->count() . ' document(s) found. <br/>';
+        foreach( $cursor as $doc ) {
+          echo "<pre>";
+          var_dump($doc);
+          echo "</pre>";
+        }
+      }
+
+        // disconnect from server
+        $m->close();
+    } catch ( MongoConnectionException $e ) {
+      die('Error connecting to MongoDB server');
+    } catch ( MongoException $e ) {
+      die('Mongo Error: ' . $e->getMessage());
+    } catch ( Exception $e ) {
+      die('Error: ' . $e->getMessage());
+    }
+    
 ?>
