7c3223385896a3931857ca6f7a69c95e44b1139d
PRiekenberg <priekenberg@oelschlaeger.de>
2022-05-10 08:45:17 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,11 @@
 <?php
 echo "<h1>Compose Test</h1>";
 require 'vendor/autoload.php';
-phpinfo();
   //try {
     // connect to Compose assuming your MONGODB_URL environment
     // variable contains the connection string
     //$connection_url = "mongodb://anwendungsprojekt:qnG4mX0QNnGbGRgcjMe3UFXVEFqiBoceoFVp39P5YUEcNLuq0uJUWC0nDtXcapvZusgQEAhlkL2qhAwrDrxDxw%3D%3D@anwendungsprojekt.mongo.cosmos.azure.com:10255/anwendungsprojektdb?ssl=true";
-    $connection_url = "mongodb://anwendungsprojektdb:h5skd43Too0CJ5f8oAHu1MemBe8Xh3VHCRAsJ4lxsOukUQmpcNlZ1yLYM7QMKtRHG0edZvcohWWNaVdcZc6IYA==@anwendungsprojektdb.mongo.cosmos.azure.com:10255/";
+    $connection_url = "mongodb://anwendungsprojektdb:h5skd43Too0CJ5f8oAHu1MemBe8Xh3VHCRAsJ4lxsOukUQmpcNlZ1yLYM7QMKtRHG0edZvcohWWNaVdcZc6IYA==@anwendungsprojektdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@anwendungsprojektdb@";
 
      // create the mongo connection object
     $client = new MongoClient($connection_url);
