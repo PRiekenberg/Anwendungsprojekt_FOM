624aaabda8ae3a523fad10744ac7c3783fc2a210
PRiekenberg <priekenberg@oelschlaeger.de>
2022-05-10 10:26:41 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,45 +1,74 @@
 <?php
+  
   require 'vendor/autoload.php';
   echo "<h1>Compose Test</h1>";
-    //try {
-      // connect to Compose assuming your MONGODB_URL environment
-      // variable contains the connection string
-      //$connection_url = "mongodb://anwendungsprojekt:qnG4mX0QNnGbGRgcjMe3UFXVEFqiBoceoFVp39P5YUEcNLuq0uJUWC0nDtXcapvZusgQEAhlkL2qhAwrDrxDxw%3D%3D@anwendungsprojekt.mongo.cosmos.azure.com:10255/anwendungsprojektdb?ssl=true";
+ 
+  /* create the mongo connection object
   $connection_url = "mongodb://anwendungsprojektdb:h5skd43Too0CJ5f8oAHu1MemBe8Xh3VHCRAsJ4lxsOukUQmpcNlZ1yLYM7QMKtRHG0edZvcohWWNaVdcZc6IYA==@anwendungsprojektdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@anwendungsprojektdb@";
+  $client = new MongoDB\Client($connection_url);
+  $collection = $client->anwendungsprojektdb->anwendungsprojektdb;
+  $db = $client->anwendungsprojektdb;
+   */
 
-    // create the mongo connection object
+  //Connect Database
+  function connectDB(){
+    $connection_url = "mongodb://anwendungsprojektdb:h5skd43Too0CJ5f8oAHu1MemBe8Xh3VHCRAsJ4lxsOukUQmpcNlZ1yLYM7QMKtRHG0edZvcohWWNaVdcZc6IYA==@anwendungsprojektdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@anwendungsprojektdb@";
     $client = new MongoDB\Client($connection_url);
+    return $client;
+  }
+
+  function getCollection() {
+    $client=connectDB();
     $collection = $client->anwendungsprojektdb->anwendungsprojektdb;
+    return $collection;
+  }
+
+  function getDB() {
+    $client=connectDB();
     $db = $client->anwendungsprojektdb;
-    /*
+    return $db;
+  }
+
+  //insert function
+  function insertDocument() {
+    $client=connectDB();
+    $collection=getCollection();
     $result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
     
     echo "Inserted with Object ID '{$result->getInsertedId()}'";
-    */
+  }
+
+  //print out list of collections
+  function printCollection() {
     echo "<h2>Collections</h2>";
     echo "<ul>";
-    // print out list of collections
+    $db = getDB();
     $cursor = $db->listCollections();
     foreach( $cursor as $doc ) {
       echo "<li>" .  $doc->getName() . "</li>";
     }
     echo "</ul>";
+  }
 
-    // print out documents in collection
   
+  // print out documents in collection
+  function printDocuments(){
+    $collection = getCollection();
     $result = $collection->find();
 
     echo "<h2>Documents in Collection</h2>";
     echo "<ul>";
     foreach ($result as $entry) {
-      echo "<li>". $entry['_id'], ': ', $entry['name'], "\n"."</li>";
+      echo "<li>", $entry['_id'], ': ', $entry['name'], "\n","</li>";
     }
     echo "/<ul>";
-    
-    
+  }
 
-      // disconnect from server
-      $client->close();
+  printCollection();
+
+  
+  // disconnect from server
+  $client->close();
 
     
 ?>
