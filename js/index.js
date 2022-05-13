e6c6f2c43094696da00f6fef772dccbad968f6ac
PRiekenberg <priekenberg@oelschlaeger.de>
2022-05-13 10:33:33 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -55,15 +55,21 @@ function read_counter_cookie(){
 
 function selectCheck(that) {
     if (that.value == "question") {
-        document.getElementById("questioncontentdiv","scenarioiddiv","phasediv").style.display = "block";
-		document.getElementById("answercontentdiv","anserstatediv","answerpointsdiv","usernamediv","passworddiv").style.display = "none";
+       // document.getElementById("questioncontentdiv").style.display = "block";
+		document.getElementById("answercontentdiv").style.display = "none";
+		document.getElementById("anserstatediv").style.display = "none";
+		document.getElementById("answerpointsdiv").style.display = "none";
+		//document.getElementById("phasediv").style.display = "block";
+		//document.getElementById("scenarioiddiv").style.display = "block";
+		document.getElementById("usernamediv").style.display = "none";
+		document.getElementById("passworddiv").style.display = "none";
     } if (that.value == "answer"){
         document.getElementById("questioncontentdiv").style.display = "none";
-		document.getElementById("answercontentdiv").style.display = "block";
-		document.getElementById("anserstatediv").style.display = "block";
-		document.getElementById("answerpointsdiv").style.display = "block";
-		document.getElementById("phasediv").style.display = "block";
-		document.getElementById("scenarioiddiv").style.display = "block";
+		//document.getElementById("answercontentdiv").style.display = "block";
+		//document.getElementById("anserstatediv").style.display = "block";
+		//document.getElementById("answerpointsdiv").style.display = "block";
+		//document.getElementById("phasediv").style.display = "block";
+		//document.getElementById("scenarioiddiv").style.display = "block";
 		document.getElementById("usernamediv").style.display = "none";
 		document.getElementById("passworddiv").style.display = "none";
     } if (that.value == "user"){
@@ -73,7 +79,7 @@ function selectCheck(that) {
 		document.getElementById("answerpointsdiv").style.display = "none";
 		document.getElementById("phasediv").style.display = "none";
 		document.getElementById("scenarioiddiv").style.display = "none";
-		document.getElementById("usernamediv").style.display = "block";
-		document.getElementById("passworddiv").style.display = "block";
+	//	document.getElementById("usernamediv").style.display = "block";
+	//	document.getElementById("passworddiv").style.display = "block";
     }
 }
