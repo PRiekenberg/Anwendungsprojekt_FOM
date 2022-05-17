4f67332c7f455a9d63590b3686e843000d71cd22
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-17 14:16:10 +0200

    Update frage.php

--- php/frage.php
+++ php/frage.php
@@ -80,13 +80,13 @@
 							value="Weiter">';
 				echo '</form>';
 				echo '</div>';
-				
+
 				$counter=1;
 				foreach ($answers as $a){
 					
 					if ($counter == 1) {
 						echo '<div id ="div_antwort'.$counter; echo '">';
-						echo '<form method="post" action="frage.php">';
+						echo '<form method="post" action="">';
 						echo '<button id="button_antwort'.$counter; echo '" name="aw'.$counter; echo '">'. $a['answercontent']; echo '<br></button>';
 						echo '<form>';
 						echo '</div>';
@@ -94,7 +94,7 @@
 						echo '<div id="div_gamescreen_center">';
 					} if ($counter == 2 or $counter == 3) {
 						echo '<div id ="div_antwort'.$counter; echo '">';
-						echo '<form method="post" action="frage.php">';
+						echo '<form method="post" action="">';
 						echo '<button id="button_antwort'.$counter; echo '" name="aw'.$counter; echo '">'. $a['answercontent']; echo '<br></button>';
 						echo '<form>';
 						echo '</div>';
@@ -102,7 +102,7 @@
 						echo '</div>';
 						echo '<div id="div_gamescreen_right">';
 						echo '<div id ="div_antwort'.$counter; echo '">';
-						echo '<form method="post" action="frage.php">';
+						echo '<form method="post" action="">';
 						echo '<button id="button_antwort'.$counter; echo '" name="aw'.$counter; echo '">'. $a['answercontent']; echo '<br></button>';
 						echo '<form>';
 						echo '</div>';
