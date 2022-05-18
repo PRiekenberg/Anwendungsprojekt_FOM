8043a27aba4bd835b18b7d5a9141516bba3ddaa3
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-18 10:05:04 +0200

    Update frage.php

--- php/frage.php
+++ php/frage.php
@@ -82,7 +82,7 @@
 							if ($counter == 1) {
 								echo '<div class="antwort '.$counter; echo '" id ="div_antwort'.$counter; echo '">';
 								echo '<label>';
-								echo '<input type="checkbox" value="Test"><span>'. $a['answercontent']; echo '</span>';
+								echo '<input type="checkbox" value="'. $a['answercontent']; echo'"><span>'. $a['answercontent']; echo '</span>';
 								echo '</label>';
 								echo '</div>';
 								echo '</div>';
@@ -90,7 +90,7 @@
 							} if ($counter == 2 or $counter == 3) {
 								echo '<div class="antwort '.$counter; echo '" id ="div_antwort'.$counter; echo '">';
 								echo '<label>';
-								echo '<input type="checkbox" value="Test"><span>'. $a['answercontent']; echo '</span>';
+								echo '<input type="checkbox" value="'. $a['answercontent']; echo'"><span>'. $a['answercontent']; echo '</span>';
 								echo '</label>';
 								echo '</div>';
 							} if ($counter == 4) {
@@ -98,10 +98,9 @@
 								echo '<div id="div_gamescreen_right">';
 								echo '<div class="antwort '.$counter; echo '" id ="div_antwort'.$counter; echo '">';
 								echo '<label>';
-								echo '<input type="checkbox" value="Test"><span>'. $a['answercontent']; echo '</span>';
+								echo '<input type="checkbox" value="'. $a['answercontent']; echo'"><span>'. $a['answercontent']; echo '</span>';
 								echo '</label>';
 								echo '</div>';
-
 							} 
 							$counter++;
 						}
