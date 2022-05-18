2c88ad3d6bd493c6d809c755c924fa06e39d8c01
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-18 11:04:55 +0200

    Update checkanswers.php

--- php/checkanswers.php
+++ php/checkanswers.php
@@ -1,8 +1,21 @@
 <?php
     require_once 'functions.php';
 	session_start();
-    
-    echo $_POST['scenarioid'];
-    echo $_POST['phase'];
-    echo $_POST['answercontent'];
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Virus zum Zusammenbauen">
+        <link rel="stylesheet" media="screen" href="css/stylesheet.css">
+    </head>
+    <body>
+        <?php
+        echo $_POST['scenarioid'];
+        echo $_POST['phase'];
+        echo $_POST['answercontent'];
+        ?>
+    </body>
