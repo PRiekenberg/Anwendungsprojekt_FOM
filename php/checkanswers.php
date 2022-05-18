64efbe3dc5164facbb5021d2f0a83f57ab956478
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-18 15:29:23 +0200

    Login

--- php/checkanswers.php
+++ php/checkanswers.php
@@ -3,7 +3,7 @@
 	session_start();
 
     if (!isset($_SESSION['username'])) {
-		die("<p>Kein Zugang<br/><a href='php/login.php'>Zum Login</a></p>");
+		die("<p>Kein Zugang<br/><a href='/php/login.php'>Zum Login</a></p>");
 	} 
 ?>
 
