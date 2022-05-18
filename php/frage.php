ae031fe2098a4db860031e1d77776f08c54437eb
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-18 09:01:18 +0200

    Update frage.php

--- php/frage.php
+++ php/frage.php
@@ -91,29 +91,29 @@
 						echo '<input type="checkbox" value="Test"><span>'. $a['answercontent']; echo '</span>';
 						echo '</label>';
 						//echo '<button id="button_antwort'.$counter; echo '" name="aw'.$counter; echo '">'. $a['answercontent']; echo '<br></button>';
-						echo '<form>';
+						//echo '<form>';
 						echo '</div>';
 						echo '</div>';
 						echo '<div id="div_gamescreen_center">';
 					} if ($counter == 2 or $counter == 3) {
 						echo '<div class="antwort '.$counter; echo '" id ="div_antwort'.$counter; echo '">';
-						echo '<form method="post" action="">';
+						//echo '<form method="post" action="">';
 						echo '<label>';
 						echo '<input type="checkbox" value="Test"><span>'. $a['answercontent']; echo '</span>';
 						echo '</label>';
 						//echo '<button id="button_antwort'.$counter; echo '" name="aw'.$counter; echo '">'. $a['answercontent']; echo '<br></button>';
-						echo '<form>';
+						//echo '<form>';
 						echo '</div>';
 					} if ($counter == 4) {
 						echo '</div>';
 						echo '<div id="div_gamescreen_right">';
 						echo '<div class="antwort '.$counter; echo '" id ="div_antwort'.$counter; echo '">';
-						echo '<form method="post" action="">';
+						//echo '<form method="post" action="">';
 						echo '<label>';
 						echo '<input type="checkbox" value="Test"><span>'. $a['answercontent']; echo '</span>';
 						echo '</label>';
 						//echo '<button id="button_antwort'.$counter; echo '" name="aw'.$counter; echo '">'. $a['answercontent']; echo '<br></button>';
-						echo '<form>';
+						echo '</form>';
 						echo '</div>';
 
 					} 
