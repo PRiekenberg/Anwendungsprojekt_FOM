abaa7ab87698ec86cb5de59512bac4d52a16e834
Patrick Kriete <patrick.kriete@outlook.de>
2022-05-20 15:02:21 +0200

    Update Buttons Menu

--- index.php
+++ index.php
@@ -39,8 +39,9 @@
 								<input type="hidden"
 								name="phase"
 								value="1">
-						<input type="submit" id="button_firmennetz"
-								value="Firmennetz">
+					<!--<input type="submit" id="button_firmennetz"
+								value="Firmennetz">-->
+						<button class =".button_menu" id="button_firmennetz" name="submit">Firmennetz</button>						
 				</form>
 				<form action="/php/frage.php">
 						<input type="hidden"
@@ -49,8 +50,9 @@
 								<input type="hidden"
 								name="phase"
 								value="1">
-						<input type="submit" id="button_krypto"
-								value="Krypto">
+						<!--<input type="submit" id="button_krypto"
+								value="Krypto">-->
+						<button class =".button_menu" id="button_krypto" name="submit">Krypto</button>
 				</form>
         	</div>
             <div id="div_gamescreen_center">
@@ -61,8 +63,9 @@
 								<input type="hidden"
 								name="phase"
 								value="1">
-						<input type="submit" id="button_social"
-								value="Social Engineering">
+						<!--<input type="submit" id="button_social"
+								value="Social Engineering">-->
+						<button class =".button_menu" id="button_social" name="submit">Social Engineering</button>
 				</form>
 				<div id="div_hacker_menu">
 					<img id="img_hacker_menu" src="images/hacker_startmenu.jpeg" alt="hacker">
@@ -76,8 +79,9 @@
 							<input type="hidden"
 							name="phase"
 							value="1">
-					<input type="submit" id="button_banking"
-							value="Online Banking">
+					<!--<input type="submit" id="button_banking"
+							value="Online Banking">-->
+					<button class =".button_menu" id="button_banking" name="submit">Online Banking</button>
 				</form>
 				<form action="/php/frage.php">
 					<input type="hidden"
@@ -88,7 +92,7 @@
 							value="1">
 					<!--<input type="submit" id="button_passwort"
 							value="Passwort">-->
-					<button id="button_password" name="submit">Passwort</button>
+					<button class =".button_menu" id="button_password" name="submit">Passwort</button>
 				</form>
             </div> 
         </div>
