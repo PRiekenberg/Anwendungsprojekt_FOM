01b17267f98a4a94ccefdd0c6344951cf22b4f35
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-20 08:31:11 +0200

    Update bestenliste.php

--- php/bestenliste.php
+++ php/bestenliste.php
@@ -40,14 +40,14 @@
                         foreach ($result as $r){
                             
                             //nur die Benutzer deren Punktzahl ungleich 0 ist
-                            //if ($r['scenario'.$_SESSION['scenarioid'].'_points'] !== 0) {
+                            if ($r['scenario'.$_SESSION['scenarioid'].'_points'] !== 0 or $r['scenario'.$_SESSION['scenarioid'].'_points'] == null) {
                                 echo '<tr>';
                                 echo '<td>' . $counter; echo '</td>';  
                                 echo '<td>' . $r['username']; echo '</td>';  
                                 echo '<td>' . $r['scenario'.$_SESSION['scenarioid'].'_points']; echo '</td>'; 
                                 echo '</tr>';
                                 $counter++;
-                            //}
+                            }
                         }
 
                     echo '</tbody>';
