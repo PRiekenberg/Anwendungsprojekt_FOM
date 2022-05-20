2fd25c9467f022d8226865b09855c7db91aede43
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-20 07:59:30 +0200

    Update bestenliste.php

--- php/bestenliste.php
+++ php/bestenliste.php
@@ -21,36 +21,42 @@
     <body>
 
         <?php 
-            echo'<h1>Bestenliste Szenario '.$_SESSION['scenarioid'];echo'</h1>';
-            echo'<table>';
-                echo'<thead>';
-                    echo '<tr>';
-                        echo '<th>Platzierung</td>';
-                        echo '<th>Username</td>';
-                        echo '<th>Punktzahl</td>';
-                    echo '</tr>';
-                echo '</thead>';
+            echo '<div align="center">';
+                echo'<h1>Bestenliste Szenario '.$_SESSION['scenarioid'];echo'</h1>';
+                echo'<table>';
+                    echo'<thead>';
+                        echo '<tr>';
+                            echo '<th>Platzierung</td>';
+                            echo '<th>Username</td>';
+                            echo '<th>Punktzahl</td>';
+                        echo '</tr>';
+                    echo '</thead>';
 
-                echo '<tbody>';
-                    
-                    $result = getallUsers();
-                    // sortiere Datensätze nach der Punktzahl absteigend für diese Szenario
-                    $result->sort(array('scenario'.$_SESSION['scenarioid'].'_points' => -1));
-                    $counter=1;
-                    foreach ($result as $r){
+                    echo '<tbody>';
                         
-                        //nur die Benutzer deren Punktzahl ungleich 0 ist
-                        //if ($r['scenario'.$_SESSION['scenarioid'].'_points'] !== 0) {
-                            echo '<tr>';
-                            echo '<td>' . $counter; echo '</td>';  
-                            echo '<td>' . $r['username']; echo '</td>';  
-                            echo '<td>' . $r['scenario'.$_SESSION['scenarioid'].'_points']; echo '</td>'; 
-                            echo '</tr>';
-                            $counter++;
-                        //}
-                    }
+                        $result = getallUsers();
+                        // sortiere Datensätze nach der Punktzahl absteigend für diese Szenario
+                        $result->sort(array('scenario'.$_SESSION['scenarioid'].'_points' => -1));
+                        $counter=1;
+                        foreach ($result as $r){
+                            
+                            //nur die Benutzer deren Punktzahl ungleich 0 ist
+                            //if ($r['scenario'.$_SESSION['scenarioid'].'_points'] !== 0) {
+                                echo '<tr>';
+                                echo '<td>' . $counter; echo '</td>';  
+                                echo '<td>' . $r['username']; echo '</td>';  
+                                echo '<td>' . $r['scenario'.$_SESSION['scenarioid'].'_points']; echo '</td>'; 
+                                echo '</tr>';
+                                $counter++;
+                            //}
+                        }
 
-                echo '</tbody>';
-			echo '</table>';
+                    echo '</tbody>';
+                echo '</table>';
+                echo '<br><br>';
+		        echo '<form>';
+                	echo '<button id="button_homescreen" formaction="/index.php">Zurück zur Startseite<br></button>';
+		        echo '</form>';
+            echo '</div>';
         ?>
     </body>
