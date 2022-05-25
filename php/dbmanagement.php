aa1a674e6142215d33f1e74a6670843cdacd07b0
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-25 17:36:02 +0200

    Update dbmanagement.php

--- php/dbmanagement.php
+++ php/dbmanagement.php
@@ -4,21 +4,21 @@
   require_once 'functions.php';
   session_start();
 	/* Kontrolle, ob innerhalb der Session */
-  //if ($_SESSION['admin'] != "true") {
-	//	die("<p>Kein Zugang<br/><a href='../index.php'>Zurück zur Startseite</a></p>");
-  //}
+  if ($_SESSION['admin'] != "true") {
+	die("<p>Kein Zugang<br/><a href='../index.php'>Zurück zur Startseite</a></p>");
+  }
 
   if (!isset($_SESSION['username'])) {
 	header("Location: /php/login.php");
 	exit();
   }
-  else{
-	$isAdmin = checkAdmin($_SESSION['username']);
-
-	if ($isAdmin == false){
-		die("<p>Kein Zugang<br/><a href='../index.php'>Zurück zur Startseite</a></p>");
-	}
-  }  
+//else{
+//	$isAdmin = checkAdmin($_SESSION['username']);
+//
+//	if ($isAdmin == false){
+//		die("<p>Kein Zugang<br/><a href='../index.php'>Zurück zur Startseite</a></p>");
+//	}
+//  }  
 
 
   if (isset($_POST['scenarioid'])) {
