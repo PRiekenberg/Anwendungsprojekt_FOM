1d5d91c64e8a77006b8f55be900f0034875564c9
Patrick Kriete <74898372+PatrickK95@users.noreply.github.com>
2022-05-25 17:40:46 +0200

    Update ende.php

--- php/ende.php
+++ php/ende.php
@@ -1,11 +1,11 @@
 <?php
-	
+	require_once 'functions.php';
 	session_start();
-	/*
+	
 	if (!isset($_SESSION['username'])) {
-		die("<p>Kein Zugang<br/><a href='php/login.php'>Zum Login</a></p>");
+		header("Location: /php/login.php");
+		exit();
 	} 
-	*/
 ?>
 
 <!DOCTYPE html>
@@ -15,41 +15,45 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Virus zum Zusammenbauen">
-        <link rel="stylesheet" media="screen" href="../css/ende.css">
-		<link rel="icon" href="/images/malware_icon.png">
+        <link rel="stylesheet" media="screen" href="/css/ende.css">
+	<link rel="icon" href="/images/malware_icon.png">
+        <title>Ende</title>
+
     </head>
     <body>
-	<div id="div_menu">
-            	<!--<a id ="logout" href="php/logout.php">Abmelden</a>-->
-		<form>
-                	<button id="button_menu" formaction="../index.php">Menü<br></button>
-		</form>
-        </div>
-        <div id = "div_h1">
-            	<h1>Modus & erfolgreich abgeschlossen</h1>
-        </div>
-	<div id="div_logout">
-            	<!--<a id ="logout" href="php/logout.php">Abmelden</a>-->
-		<form>
-                	<button id="button_logout" formaction="php/logout.php">Abmelden<br></button>
-		</form>
+		<div id="div_menu">
+			<form>
+					<button id="button_menu" formaction="../index.php">Menü<br></button>
+			</form>
+		</div>
+		<div id = "div_h1">
+			<h1>Fehler:</h1>
         </div>
         <div id = "div_h2">
-        	<h2>Sie haben & Punkte von & möglichen Punkte erreicht</h2>
-        </div>
-        <div id="div_gamescreen">
-        </div>
-        <div id="div_hinweis">
-            	<a id ="hinweis">
-                	Hinweis!
-                	<br>
-                	Dies ist keine Webseite um einen echten Virus zu bauen!
-            	</a>
-        </div>
-        <div id ="div_copyright">
-            	<a id="copyrigth">
-                	&copy Gruppe 7: Virus zum zusammenbauen
-            	</a>
-	</div>
+            <h2>Es sind keine Fragen für dieses Szenario vorhanden!</h2>
+	    </div>
+		<div id="div_logout">
+			<form>
+					<button id="button_logout" formaction="/php/logout.php">Abmelden<br></button>
+			</form>
+		</div>
+		<div id="div_gamescreen">
+            <a id="error">
+                ¯\_(ツ)_/¯
+            </a>
+        </div>  		
+		<div id="div_hinweis">
+			<a id ="hinweis">
+				Hinweis!
+				<br>
+				Dies ist keine Webseite um einen echten Virus zu bauen!
+			</a>
+		</div>
+		<div id ="div_copyright">
+			<a id="copyright">
+				&copy Gruppe 7: Virus zum zusammenbauen
+			</a>
+		</div>
+		
     </body>
 </html> 
