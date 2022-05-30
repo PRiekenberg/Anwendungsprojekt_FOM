b76e51e2b523dc396b23b3056365db8e4b0e6fd2
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-30 08:24:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 		exit();
 	} 
 
-	require_once '/php/functions.php';
-	require_once '/vendor/autoload.php';
+	//require_once '/php/functions.php';
+	//require_once '/vendor/autoload.php';
 	$_SESSION['scenario1_phase']=getUsersPhases($_SESSION['username'],"1");
 	$_SESSION['scenario2_phase']=getUsersPhases($_SESSION['username'],"2");
 	$_SESSION['scenario3_phase']=getUsersPhases($_SESSION['username'],"3");
