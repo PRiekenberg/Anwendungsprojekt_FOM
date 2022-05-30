457cf54d20bc098652d8bbe60b936354be66b436
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-30 08:33:33 +0200

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -1,6 +1,8 @@
 <?php
   
-  require_once '/vendor/autoload.php';
+  //require_once '/vendor/autoload.php';
+
+  require_once '/home/site/wwwroot/vendor/autoload.php';
  
   //Connect Database
   function connectDB(){
