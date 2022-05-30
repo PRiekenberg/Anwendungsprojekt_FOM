6575de336a23626dbaf384c84e7022f4c9c5ca42
PRiekenberg <85277173+PRiekenberg@users.noreply.github.com>
2022-05-30 20:15:16 +0200

    Update checkanswers.php

--- php/checkanswers.php
+++ php/checkanswers.php
@@ -78,57 +78,6 @@
 							echo '</a>';
 						?>
 					</div>
-					<div id="div_button_next">
-						<?php
-							foreach($gegebeneantworten as $antwort) {
-								//prüfe ob gegebene Antwort in Array mit richtigen Antworten
-
-								if ($antwort != null) {
-									
-									//punkte für die aktuelle Antwort abrufen
-									$answerpoints = queryAnswersPoints($_SESSION['scenarioid'],$_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'],$antwort);
-
-									if (($key = array_search($antwort, $richtigeantworten)) !== false) {
-										unset($richtigeantworten[$key]);
-										setUserPoints($_SESSION['scenarioid'],10,$_SESSION['username']);
-									} else {
-										$fehlergemacht=1;
-
-										setUserPoints($_SESSION['scenarioid'],-5,$_SESSION['username']);
-									}
-								} 
-							}
-							
-							//nicht alle richtigen Antworten waren dabei
-							if (count($richtigeantworten) > 0) {
-								$punkte=count($richtigeantworten)*-5;
-								setUserPoints($_SESSION['scenarioid'], $punkte, $_SESSION['username']);
-							} 
-
-
-								echo '<div id="div_nextphase">';
-								echo '<form action="/php/frage.php">';
-									$_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'] = $_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'] + 1;
-
-									$new_phase = $_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'];
-									$scenarioid = $_SESSION['scenarioid'];
-
-									setUserPhase($scenarioid,$_SESSION['username'],$new_phase);
-									
-									echo '<input type="hidden"';
-									echo 'name="scenarioid"';
-									echo 'value="'.$scenarioid; echo'">';
-									echo '<input type="hidden"';
-									echo 'name="phase"';
-									echo 'value="'.$new_phase; echo '">';
-								
-									echo '<input type="submit" id="button_next"
-											value="Weiter zur nächsten Frage">';
-									echo '</form>';
-								echo '</div>'; 
-
-						?>
-					</div>
 						<?php
 							$counter=1;
 							foreach ($answers as $a){
@@ -201,5 +150,53 @@
 		</div>
 
 
-        
+        <?php
+            foreach($gegebeneantworten as $antwort) {
+                //prüfe ob gegebene Antwort in Array mit richtigen Antworten
+
+                if ($antwort != null) {
+                    
+                    //punkte für die aktuelle Antwort abrufen
+                    $answerpoints = queryAnswersPoints($_SESSION['scenarioid'],$_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'],$antwort);
+
+                    if (($key = array_search($antwort, $richtigeantworten)) !== false) {
+                        unset($richtigeantworten[$key]);
+                        setUserPoints($_SESSION['scenarioid'],10,$_SESSION['username']);
+                    } else {
+                        $fehlergemacht=1;
+
+                        setUserPoints($_SESSION['scenarioid'],-5,$_SESSION['username']);
+                    }
+                } 
+            }
+              
+              //nicht alle richtigen Antworten waren dabei
+              if (count($richtigeantworten) > 0) {
+                  $punkte=count($richtigeantworten)*-5;
+                  setUserPoints($_SESSION['scenarioid'], $punkte, $_SESSION['username']);
+              } 
+
+
+                  echo '<div id="div_nextphase">';
+                  echo '<form action="/php/frage.php">';
+                      $_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'] = $_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'] + 1;
+
+                      $new_phase = $_SESSION['scenario'.$_SESSION['scenarioid'].'_phase'];
+                      $scenarioid = $_SESSION['scenarioid'];
+
+                      setUserPhase($scenarioid,$_SESSION['username'],$new_phase);
+                      
+                      echo '<input type="hidden"';
+                      echo 'name="scenarioid"';
+                      echo 'value="'.$scenarioid; echo'">';
+                      echo '<input type="hidden"';
+                      echo 'name="phase"';
+                      echo 'value="'.$new_phase; echo '">';
+                  
+                      echo '<input type="submit" id="button_next"
+                              value="Weiter zur nächsten Frage">';
+                      echo '</form>';
+                  echo '</div>'; 
+
+        ?>
     </body>
